011913bc0191d56b004cff471ba91e4b8402b58f
ksomerset <ksomerset966@gmail.com>
2019-01-02 16:18:43 -0500

    Delete plot2.R

--- plot2.R
+++
@@ -1,4 +0,0 @@
-power$Time <- as.numeric(power$Time)
-plot(powersubset$Time, powersubset$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
-dev.copy (png, file = "plot2.png", width=480, height=480)
-dev.off()
